843423ddd954fdfa96cd60b63c86bc611d5a286a
Dendrobyte <markbacon78@gmail.com>
2020-04-10 18:38:13 -0400

    Teleportation disabled if outside adv worlds

--- src/main/java/com/oinkcraft/oinkutils/creativespecmode/SpecModeTeleportListener.java
+++ src/main/java/com/oinkcraft/oinkutils/creativespecmode/SpecModeTeleportListener.java
@@ -1,12 +1,11 @@
 package com.oinkcraft.oinkutils.creativespecmode;
 
 import com.oinkcraft.oinkutils.Main;
-import org.bukkit.ChatColor;
-import org.bukkit.GameMode;
-import org.bukkit.Location;
+import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
@@ -22,20 +21,28 @@ public class SpecModeTeleportListener implements Listener {
     String advBuilderString = Main.getInstance().getConfig().getString("advbuilder-world");
 
     @EventHandler
-    public void onAdvBuilderWorldPlayerTeleport(PlayerTeleportEvent event){
-        Location to = event.getTo();
-
+    public void onAdvBuilderWorldPlayerTeleport(PlayerChangedWorldEvent event){
+        Player player = event.getPlayer();
+        World to = player.getWorld();
+        World from = event.getFrom();
         // We only check the world they are arriving in because PerWorldInv / Multiverse should manage them leaving
-        if(to.getWorld().getName().toLowerCase().contains(advBuilderString)){
-            Player player = event.getPlayer();
+        if(to.getName().toLowerCase().contains(advBuilderString)){
             if(!player.hasPermission("oinkutils.advbuilder.build")){
-                player.sendMessage("" + ChatColor.BLUE + ChatColor.ITALIC + "Welcome to the Advanced Builder world! Enjoy looking at the builds :)");
+                player.sendMessage("" + ChatColor.BLUE + ChatColor.ITALIC + "Welcome to the Advanced Builder world. Enjoy the builds!");
                 BukkitTask quickTimer = new BukkitRunnable() {
                     @Override
                     public void run() {
                         player.setGameMode(GameMode.SPECTATOR);
                     }
                 }.runTaskLater(Main.getInstance(), 20L);
+            }
+        }
+
+        if(from.getName().toLowerCase().contains(advBuilderString)){
+            String spawnWorldName = Main.getInstance().getConfig().getString("spawn-world");
+            if(!to.getName().equalsIgnoreCase(spawnWorldName)){
+                player.teleport(Bukkit.getServer().getWorld(spawnWorldName).getSpawnLocation());
+                player.sendMessage("" + ChatColor.GRAY + "You have been sent to the default spawn world as to avoid abuse of spectator mode teleportation.");
             }
         }
     }
